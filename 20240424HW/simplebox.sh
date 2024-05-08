31b4dc1c4565672ec6f239bbbfff80292cf9332b
Draw0919 <102810421+Draw0919@users.noreply.github.com>
2024-05-08 17:05:54 +0800

    Update simplebox.sh

--- 20240424HW/simplebox.sh
+++ 20240424HW/simplebox.sh
@@ -1,70 +1,54 @@
 #!/bin/bash
 
 # 使用說明
-# 1. 請將此腳本複製到您的 Ubuntu 系統中。
-# 2. 在終端機中，使用 cd 命令導航到腳本所在的目錄。
-# 3. 使用 chmod +x filename.sh 命令賦予腳本執行權限（其中 filename.sh 是您複製的腳本文件的名稱）。
-# 4. 執行腳本，可以使用 ./filename.sh 命令（同樣，filename.sh 是您的腳本文件的名稱）。
+# 1. 將此腳本複製到您的Ubuntu容器中。
+# 2. 開啟終端機。
+# 3. 使用cd命令導航到腳本所在的目錄。
+# 4. 給予腳本執行權限：chmod +x setup_script.sh
+# 5. 執行腳本：./setup_script.sh
 
 # 靶機管理設定
 echo ""
 echo "靶機管理設定"
-# 新增使用者 "admin"，並將其添加到 sudo 群組，以便具有管理權限
 useradd -m admin
-usermod -aG sudo admin
-# 更改 admin 的密碼為明文 "admin"
+usermod -aG root admin
 echo "admin:admin" | chpasswd
-# 檢查是否已安裝 SSH 服務，如果未安裝，則進行安裝
-if [ ! -x /usr/sbin/sshd ]; then
+if ! service ssh status > /dev/null 2>&1; then
     apt-get update
-    apt-get install -y openssh-server
+    apt-get install -y ssh
 fi
-service ssh restart
+service ssh start
 
 # 外部滲透設定
 echo ""
 echo "外部滲透設定"
-# 新增一個名為 "gary" 的一般用戶，並設置其密碼為 "password"
-useradd gary
+useradd -m gary
 echo "gary:password" | chpasswd
-# 在 gary 的家目錄中創建一個名為 user.txt 的文件，並將字符串 "user's flag" 添加到文件中
 echo "user's flag" > /home/gary/user.txt
 
 # 內部提權設定
 echo ""
 echo "內部提權設定"
-# 檢查是否已安裝 sudo，如果未安裝，則進行安裝
-if ! dpkg -l | grep -q sudo; then
+echo "root's flag" > /root/root.txt
+if ! command -v sudo > /dev/null 2>&1; then
     apt-get update
     apt-get install -y sudo
 fi
-# 授權所有使用者可以以 root 權限執行 cp 命令
-echo "ALL ALL=(root) NOPASSWD: /bin/cp" >> /etc/sudoers
-# 在 root 的家目錄中創建一個名為 root.txt 的文件，並將字符串 "root's flag" 添加到文件中
-echo "root's flag" > /root/root.txt
+echo "ALL ALL=(ALL) NOPASSWD: /bin/cp" >> /etc/sudoers
 
-# 創建 10 個不同的 .txt 檔案，每個文件的內容都不同，並使用隨機哈希加密
-for i in {1..10}; do
-    # 生成隨機的檔名
-    filename="file$i.txt"
-    # 生成隨機的內容
-    content=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 10)
-    # 使用隨機哈希加密內容
-    hashed_content=$(echo "$content" | sha256sum | awk '{print $1}')
-    # 將加密後的內容寫入文件
-    echo "$hashed_content" > "/root/$filename"
+# 創建加密文件
+echo ""
+echo "創建加密文件"
+for i in {1..50}; do
+    file="/home/user/secret_file_$i.txt"
+    echo "Data for file $i" | md5sum | cut -d' ' -f1 > "$file"
+    chmod 600 "$file"
 done
-
-# 隨機選擇一個檔案，將其內容設置為 root 的標誌並與密碼哈希值匹配
-# 取得 root 的密碼哈希值
-root_password_hash=$(grep '^root:' /etc/shadow | cut -d':' -f2)
-# 隨機選擇一個檔案
-random_file=$(ls /root/*.txt | shuf -n 1)
-# 將 root 的標誌寫入所選檔案
-echo "root's flag" > "$random_file"
-# 將檔案的名稱和密碼哈希值存入一個文件，以便管理者查看
-echo "File: $random_file, Password Hash: $root_password_hash" > /root/password_hash_mapping.txt
+# 將root.txt文件的hash與一個文件的hash匹配
+hash_of_root=$(md5sum /root/root.txt | cut -d' ' -f1)
+echo $hash_of_root > "/home/user/secret_file_with_root_hash.txt"
 
 # 完成設定
 echo ""
 echo "完成設定"
+echo "腳本執行完成，靶機已設定好基本漏洞。"
